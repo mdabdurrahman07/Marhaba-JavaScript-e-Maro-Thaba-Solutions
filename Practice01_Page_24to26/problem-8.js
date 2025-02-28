dab8ed1ec9dae838c6207322545ff8d3a66d71f6
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-01 01:24:52 +0600

    Practice01_page24to26 problem 8 solution

---
+++ Practice01_Page_24to26/problem-8.js
@@ -0,0 +1,9 @@
+// nicher symbolgulo diye ekti shonkhar sathe arekti shonkhar tulona kora hocche| aishob tulonar modhe konta shotikh ar konta bhul ?
+
+// ANS: 
+
+//  15 > 12 : 15 greater than 12 its True 
+
+//  4 < 10 : 4 less than 10 its True
+
+// 7 > 9 : 7 greater than 9 is False 
