d06c202d8714239aa630dd7e2a2255257bd91691
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-01 01:20:34 +0600

    Practice01_page24to26 problem 7 solution

---
+++ Practice01_Page_24to26/problem_7.js
@@ -0,0 +1,8 @@
+// 450 taka jodi 8 joner modhe bhaag kore dite hoy, tahole porthteke koto taka pabe ? ar jodi kew vangti poisha na neai , tahole tor  haate koto taka baki thakbe ?
+
+// ANS: 
+
+// Jodi 450 taka 8 joner modhe shoman bhaage bhaag kori tahole shobai pabe (450/8)taka = 56.25taka
+
+// Jodi kew vangti poisha na neai tahole amr haate thakbe  (.25 X 8) = 2 taka
+
