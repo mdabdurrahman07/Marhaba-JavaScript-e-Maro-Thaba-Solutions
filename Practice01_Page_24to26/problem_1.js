31dc411f7071fc20439ef5727e5ce30217de7c9b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-01 00:32:25 +0600

    Practice01_page24to26 problem 1 solution

---
+++ Practice01_Page_24to26/problem_1.js
@@ -0,0 +1,8 @@
+// Nicher shonkha gulor modhe konta positive ar konta negative ?
+
+// -5, 12, -8, 0, 3.5
+
+// Positive: 12, 3.5
+// Negative: -5 ,-8
+
+// 0 ekti niropekho shonkha ja positive o na abar negative o na!!
