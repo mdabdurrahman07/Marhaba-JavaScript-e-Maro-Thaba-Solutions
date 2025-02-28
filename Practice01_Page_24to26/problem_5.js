c1cd17d0feca509b80b68f1bb5271baef70b72e1
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-01 01:14:48 +0600

    Practice01_page24to26 problem 6 solution

--- Practice01_Page_24to26/problem_5.js
+++ Practice01_Page_24to26/problem_5.js
@@ -1,3 +1,3 @@
 // tui jodi 64 takar kichu kine 100 taka dis, taholee dokandar koto taka ferot dibe ?
 
-// ANS: 36 taka
+// ANS: (100-64) taka = 36 taka
---
+++ Practice01_Page_24to26/problem_6.js
@@ -0,0 +1,3 @@
+// paachjoner prothteke jodi 350 taka kore eid bonus deya hoy, tahole moth khoroch koto hobe ?
+
+// ANS: (350 x 5)taka = 1750taka
