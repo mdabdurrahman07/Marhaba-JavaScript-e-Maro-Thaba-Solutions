5b55d821fd9906836f354b8937d7127192de2e27
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-01 01:40:51 +0600

    Practice01_page24to26 problem 10 solution

---
+++ Practice01_Page_24to26/problem_10.js
@@ -0,0 +1,6 @@
+/** toke ekta formula deya holo| formulate bola aache, chaktir khetrofol hobe π x r2 | aakhane π er maan π = 3.14 ar r holo baashardo| ar aitao bole deya aach e r2 mane r-er 2bar ghun korte hobe| bola aache, 
+
+chaktir baashardo r er maan 7 | akhon tui ki khetrofol ber korte parbi ?
+*/ 
+
+// ANS:- {3.14 x (7x7)} = 153.86
