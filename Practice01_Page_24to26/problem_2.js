9bf1bb17f6cd972e85bfa906efb82c3ab0109064
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-01 00:50:50 +0600

    Practice01_page24to26 problem 2 solution

---
+++ Practice01_Page_24to26/problem_2.js
@@ -0,0 +1,9 @@
+// PurnoShonkha JorrShonkha, ar BijorrShonkha alada kore chinito koro:
+
+// Shonkha: 23,56,19.69,101,42,89,11.37
+
+// PurnoShonkha: 23,56,101,42,89
+
+// JorrShonkha: 56,42
+
+// BijorrShonkha: 23, 101, 89
