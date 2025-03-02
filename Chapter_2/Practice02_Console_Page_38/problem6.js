3269f7ec68cfb8fbd34f36f16ef0348aa224b31d
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-03 00:28:41 +0600

    Chapter 2 Console Practice 6 solution

---
+++ Chapter_2/Practice02_Console_Page_38/problem6.js
@@ -0,0 +1,7 @@
+// tor prio team opposite teamke shorboccho koita goal diche ek match a| sheita diye goals name ekti variable likhe sheitake output hisabe dekha
+
+// ANS
+
+const goals = 5;
+
+console.log(goals)
