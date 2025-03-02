a46a88f180f9a00ba6c453b33759f319049ecd99
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-03 00:21:40 +0600

    Chapter 2 Console Practice 2 & 3 solution

---
+++ Chapter_2/Practice02_Console_Page_38/problem2.js
@@ -0,0 +1,7 @@
+// tui ai bochor je koita boi porar porikolpona korchos, sheita booksToRead namer ekti variable a rakh | pore booksToRead er maan console output dekha
+
+// ANS
+
+const booksToRead = 5;
+
+console.log(booksToRead)
---
+++ Chapter_2/Practice02_Console_Page_38/problem3.js
@@ -0,0 +1,7 @@
+// tui protidin sokal koitai shomoy ghum theke uthos, shetakee wakeUpTime name ekti variabley declare kor | tarpor wakeUpTime er notun man set kore abong consoley output hisabe dekh
+
+// ANS
+
+const wakeUpTime = 11
+
+console.log(wakeUpTime)
