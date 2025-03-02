14d8cc80f2b00fafd157ba8ad3e6d6842963e58a
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-03 00:23:45 +0600

    Chapter 2 Console Practice 4 solution

---
+++ Chapter_2/Practice02_Console_Page_38/problem4.js
@@ -0,0 +1,7 @@
+// aaj saradine koi ghonta shomoy tui mobilephone nosto korchos, sheitake wastedTime name ekti variable ai maan rakh | tarpor aitake console dekha
+
+// ANS 
+
+const wastedTime = 2;
+
+console.log(wastedTime)
