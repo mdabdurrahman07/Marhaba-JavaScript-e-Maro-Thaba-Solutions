db15e784bf4408fc20ceb5af0ee78818030331d2
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-03 23:50:34 +0600

    Chapter 2 String Practice 1 solution

---
+++ Chapter_2/Practice02_String_Page_41/problem1.js
@@ -0,0 +1,7 @@
+// akhon name name ekti variable declare kor, ar jeitar maan hobe tor nam| erpor sheitake console a output hisabe dekha jabe
+
+// ANS
+
+const name = "MD Abdur Rahman Nur Jamil";
+
+console.log(name)
