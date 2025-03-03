108d17fb182bcb12ae0c0b0359bec0191f2d2904
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 00:02:59 +0600

    Chapter 2 String Practice 2 solution

---
+++ Chapter_2/Practice02_String_Page_41/problem2.js
@@ -0,0 +1,7 @@
+// tor thikana (address) ekti string variable declare kore dekh| ar shietake console.log() diye output dekh|
+
+// ANS 
+
+const address = "Dhaka, Bangladesh";
+
+console.log(address);
