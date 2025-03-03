0229fe2e8ccb173eaed7e1ce362238270cf8ed61
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 00:05:53 +0600

    Chapter 2 String Practice 3 solution

---
+++ Chapter_2/Practice02_String_Page_41/problem3.js
@@ -0,0 +1,7 @@
+// tor best friend kon school, college ba versity pore , shei school, collge ba vversity name ekti varibale declare kor tarpor shietake variable a declare kor
+
+// ANS
+
+const University = "Daffodil International of IT"
+
+console.log(University)
