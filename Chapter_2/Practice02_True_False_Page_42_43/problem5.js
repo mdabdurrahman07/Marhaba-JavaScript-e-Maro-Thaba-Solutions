af4671749a9057f8c89ecb2968c39e80c3f122d8
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:28:29 +0600

    Chapter 2 True False Practice 5 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem5.js
@@ -0,0 +1,7 @@
+// tor ajke pora sesh hoyeche ki na ? pora sesh hole true , na hole false hobe | ar sheta rakahr jonno isStudyDone name ekti boolean variable declare kor | 
+
+// ANS
+
+const isStudyDone = true;
+
+console.log(isStudyDone);
