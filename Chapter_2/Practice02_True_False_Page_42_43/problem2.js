786d7f66f4d106bda7d0832b326acb651f6bc645
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:18:56 +0600

    Chapter 2 True False Practice 2 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem2.js
@@ -0,0 +1,7 @@
+// tui nije ekti program likhe dekh, tor crush toke message diyeche kina na | dhor message true , na dile false | sheita print kor console.log() diye | 
+
+// ANS
+
+const isCrushReplied = true;
+
+console.log(isCrushReplied);
