74a861699ed104bfd0769df62304204653a07dc5
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:32:51 +0600

    Chapter 2 True False Practice 7 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem7.js
@@ -0,0 +1,7 @@
+// tor ki raate khawa sesh ? seta niye isDinnerDone name ekti boolean variable declare kor | khawa sesh hole ki likhbi ar sesh na hole ki likhbi, sheta toh janos already |
+
+// ANS
+
+const isDinnerDone = true;
+
+console.log(isDinnerDone);
