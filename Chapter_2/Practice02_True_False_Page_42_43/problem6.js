0c2ad22f40ea6cce703a7cba6839ddf79323c98b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:30:13 +0600

    Chapter 2 True False Practice 6 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem6.js
@@ -0,0 +1,7 @@
+// akhon isPhoneCharged name ekti boolean variable declare kor | charge sesh hole true , sesh na hole false| 
+
+//  ANS
+
+const isPhoneCharged = false;
+
+console.log(isPhoneCharged);
