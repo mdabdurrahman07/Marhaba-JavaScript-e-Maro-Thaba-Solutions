ef4288ec615b46f929899376ebe1ec12767720f5
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:21:14 +0600

    Chapter 2 True False Practice 3 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem3.js
@@ -0,0 +1,7 @@
+// tor prio team khelai jite gelo, akhon shei khusite bogol bajate bajate celebrating name ekti boolean variable declare kore sheita man dea, ar output dekh| 
+
+//  ANS
+
+const celebrating = true;
+
+console.log(celebrating);
