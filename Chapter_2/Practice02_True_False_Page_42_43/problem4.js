10d91ef4392bad8c349b80e91f0155296b5df1bb
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:26:14 +0600

    Chapter 2 True False Practice 4 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem4.js
@@ -0,0 +1,14 @@
+// tor akhon boolean type shomporkhe bhalo dharona hoyeche| aibar ekti program likh, jekhane tui 3ti boolean declare korbi: "isHappy", "hasMoney", ar "isHungry" | ai 3tir man jothakrome true,false ar true set korbi| tarpr output de console.log() diye |
+
+// ANS
+
+const isHappy = true;
+
+const hasMoney = false;
+
+const isHungry = true;
+
+
+console.log(isHappy);
+console.log(hasMoney);
+console.log(isHungry);
