e34f4899b3130d83dc26a52266bb7e7eb1102a7d
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-04 23:16:21 +0600

    Chapter 2 True False Practice 1 solution

---
+++ Chapter_2/Practice02_True_False_Page_42_43/problem1.js
@@ -0,0 +1,7 @@
+// dhor tor bondhu bambu khaise| akhn shay happy kina ekti boolean typer variable declare kore seitar man rakh | ar sheita console a print kor
+
+//  ANS
+
+const happy = false;
+
+console.log(happy)
