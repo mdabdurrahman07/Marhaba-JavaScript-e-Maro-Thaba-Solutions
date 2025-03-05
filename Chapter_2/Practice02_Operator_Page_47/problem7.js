890791c35093693bd17a24ec6411232cc89ead9b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-06 00:40:28 +0600

    Chapter 2 Operator  Practice 7 solution

---
+++ Chapter_2/Practice02_Operator_Page_47/problem7.js
@@ -0,0 +1,11 @@
+// tui 8 ke 3 diye bhaag dile bhaag fol koto ar bhaag sesh koto thake, sheta ber korar jonno ekti program likh |
+
+// ANS
+
+const divide = 8/3;
+
+console.log(divide);
+
+const mod = 8%3;
+
+console.log(mod);
