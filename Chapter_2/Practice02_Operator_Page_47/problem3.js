4bcf512e6f650de11bfb6aaae6e55925b42658e9
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-06 00:24:38 +0600

    Chapter 2 Operator  Practice 3 solution

---
+++ Chapter_2/Practice02_Operator_Page_47/problem3.js
@@ -0,0 +1,13 @@
+// tor kache 60 taka aache| tui jodi protibar 15 taka kore khoroch korish, koybar khorch korte parbi ? abong khoroch er por koy taka baki thakbe shieta ber korte ekta programme lekh |
+
+const haveMoney = 60;
+
+const usageOfTaka_A_Time = 15;
+
+const totalUsage = haveMoney / usageOfTaka_A_Time;
+
+console.log(totalUsage);
+
+const leftOverMoney = haveMoney - (totalUsage*usageOfTaka_A_Time);
+
+console.log(leftOverMoney);
