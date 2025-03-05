ead13cbd22d973c985232f6230acaae9d963d83f
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-06 00:33:13 +0600

    Chapter 2 Operator  Practice 5 solution

---
+++ Chapter_2/Practice02_Operator_Page_47/problem5.js
@@ -0,0 +1,11 @@
+// tui jodi 153 kg chal ar 261 kg dal kinli, tahole tui koy kg jinish kinechis? mot kg ber korar ekti program likh| 
+
+// ANS
+
+const chal = 153;
+
+const dal = 261;
+
+const totalKg = chal+dal;
+
+console.log(totalKg);
