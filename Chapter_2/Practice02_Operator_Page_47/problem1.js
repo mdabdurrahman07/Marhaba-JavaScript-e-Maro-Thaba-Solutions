c42d80fef9abb5ee594d380abda46addfc402609
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-06 00:16:36 +0600

    Chapter 2 Operator  Practice 1 solution

---
+++ Chapter_2/Practice02_Operator_Page_47/problem1.js
@@ -0,0 +1,11 @@
+// dhor tui 100 taka ay korechis| tar modhe 40 taka khoroch korchis | baki taka koto roilo, seta ber korar ekti program lekh
+
+// ANS 
+
+const Income = 100;
+
+const expense = 40;
+
+const leftOver = Income - expense;
+
+console.log(leftOver);
