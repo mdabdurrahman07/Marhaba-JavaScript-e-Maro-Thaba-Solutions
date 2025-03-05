48435df1f427fb4b21c9b31b692f29a5ad704c4d
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-06 00:50:31 +0600

    Chapter 2 Operator  Practice 9 & 10 solution

---
+++ Chapter_2/Practice02_Operator_Page_47/problem10.js
@@ -0,0 +1,15 @@
+// dhor, ekdin ghum theke uthe khidar chote tui chotokhato ekti rakkosh hoye gsos| tarpor theke protidin 4kg chaler bhaat kheye felos| akhon jodi toke 12 mon(480kg) chal deya hoi thake, tahole ai chal diye tor koi din jabe| abar ekhmash jodi 30 dine hoy, tahole ai 12 mon chal diye tor koto mash jabe |
+
+// ANS
+
+const totalChal = 480;
+
+const dailyUsage = 4
+
+const days = totalChal / dailyUsage;
+
+console.log(days);
+
+const months =  days / 30;
+
+console.log(months);
---
+++ Chapter_2/Practice02_Operator_Page_47/problem9.js
@@ -0,0 +1,7 @@
+// tui "Bangla" ar "desh" string jogg kore "Bangladesh" banate chas | kibhabe korbi sheta dekhanor program likh|
+
+// ANS
+
+const output = "Bangla"+"desh";
+
+console.log(output);
