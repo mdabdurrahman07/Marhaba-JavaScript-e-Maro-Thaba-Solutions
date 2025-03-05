8225e348468e7faadd05c6daa010bb0bbd09fc85
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-06 00:19:38 +0600

    Chapter 2 Operator  Practice 2 solution

---
+++ Chapter_2/Practice02_Operator_Page_47/problem2.js
@@ -0,0 +1,11 @@
+// tor kache 10ta oencil aache| protibar tui ekhsathe 2ta pencil bebohar korbi | koto bar bebohar korte parbi ? programme likhe ber kor |
+
+// ANS
+
+const pencil = 10;
+
+const usageOfPencilAt_A_Time = 2;
+
+const totalUsage = pencil / usageOfPencilAt_A_Time;
+
+console.log(totalUsage);
