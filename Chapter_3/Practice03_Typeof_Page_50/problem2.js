904680eac3e99e063a3b681030ac5701d6fb52b6
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:24:55 +0600

    Chapter Typeof Practice 2 solution

---
+++ Chapter_3/Practice03_Typeof_Page_50/problem2.js
@@ -0,0 +1,7 @@
+// ekti variable "learning" declare kor, jar maan true, ar sheta console.log() diye output kor| pore "typeof" bebohar kore dekh,eta ki dhoroner variable|
+
+// ANS
+
+const learning = true;
+
+console.log(typeof learning);
