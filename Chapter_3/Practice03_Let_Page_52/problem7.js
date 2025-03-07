4fa9661bafd7d216ff5f26bf13a8f21732f425bd
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 16:25:51 +0600

    Chapter 3 Let Practice 7 solution

---
+++ Chapter_3/Practice03_Let_Page_52/problem7.js
@@ -0,0 +1,9 @@
+// tui ajke 3 ghonta dekha korar porikolpona korechis, playTime name ekti variable rakh | pore playTime er maan update kore asholey tui koto ghonta khela korechis, sheti set kor| jodi tui khelish , tahole playTime er maan 0 set kor abong console dekh |
+
+let playTime;
+
+playTime = 0;
+
+console.log(playTime
+
+);
