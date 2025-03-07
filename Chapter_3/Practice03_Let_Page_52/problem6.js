a8b465a9e58a8b324777b8ba7fa15f841cb65c7e
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 16:20:48 +0600

    Chapter 3 Let Practice 6 solution

---
+++ Chapter_3/Practice03_Let_Page_52/problem6.js
@@ -0,0 +1,13 @@
+// toder classe 40ta chair aache| koydin por 2ta chair er paa bhenge gelo| chairsCount name ekti variable ai maan update kor | aro koidin por pasher room er senior bhaira eshe 6ta chair niye geche| aibaro chairsCount er maan update kor abong console final maan output kore dekh |
+
+// ANS
+
+let chairsCount = 40;
+
+chairsCount = 40-2;
+
+chairsCount = 38-6;
+
+console.log(
+    chairsCount
+);
