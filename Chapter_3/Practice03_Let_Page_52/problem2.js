fe56290f102f4bf83b24dfb4ebb3c731d772d939
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:56:00 +0600

    Chapter 3 Let Practice 2 solution

---
+++ Chapter_3/Practice03_Let_Page_52/problem2.js
@@ -0,0 +1,7 @@
+// tor pocket akhon 500 taka aache, pocketMoney name ekti variable rakh| pore pocketMoney er man 150 set kor abong console print kor|
+
+let pocketMoney;
+
+pocketMoney = 150;
+
+console.log(pocketMoney);
