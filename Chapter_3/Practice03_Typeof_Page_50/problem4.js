277c396b5161070eee47d0393b4a487c7528f9f5
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:30:59 +0600

    Chapter Typeof Practice 4 solution

---
+++ Chapter_3/Practice03_Typeof_Page_50/problem4.js
@@ -0,0 +1,7 @@
+// khub kathfata gorome kopale haat dekhli, ga purre jay jay obostah| shei muhurte tui "temperature" name ekti variable declare korli, jar maan 102.5| aita ki dhoroner variable, sheta "typeof diye" check kor|
+
+// ANS
+
+const temperature = 102.5;
+
+console.log(typeof temperature);
