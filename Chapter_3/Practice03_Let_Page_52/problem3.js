147146c0bce5dff353cd35489e4545eaa1a6a35a
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 16:00:08 +0600

    Chapter 3 Let Practice 3 solution

---
+++ Chapter_3/Practice03_Let_Page_52/problem3.js
@@ -0,0 +1,9 @@
+// tui 4 ghonta porashuna korar porikolpona korechis, studyTime name ekti variable rakh| pore studyTime er maan update kore tui asholey ajke joto ghonta porashuna korchos, sheta set kor| maan josi shunno hoy, tahole 0-e set kor abong consoley dekh| 
+
+let studyTime;
+
+studyTime = 0;
+
+console.log(
+    studyTime
+);
