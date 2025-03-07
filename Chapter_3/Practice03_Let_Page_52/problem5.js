2fe7e97611acf185607f7dfdca00055392c2532e
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 16:14:50 +0600

    Chapter 3 Let Practice 5 solution

---
+++ Chapter_3/Practice03_Let_Page_52/problem5.js
@@ -0,0 +1,9 @@
+// tui ekti notun phone kinchili 25000 taka diye| sheta niye ekti variable declare kor | ar ekh mash por shei phone bikri korte giyey dekhos dam dite chai 12000 matro, ki korbi! phonePrice name ekti variable maan prothome 25000 dibi |  tarpor shetake 12000 diye update kor abong console dekha|
+
+// ANS
+
+let phonePrice = 25000;
+
+phonePrice = 12000;
+
+console.log(phonePrice);
