8a42743db810160a0c5afe5271b9f40395bea9b7
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:21:32 +0600

    Chapter Typeof Practice 1 solution

---
+++ Chapter_3/Practice03_Typeof_Page_50/problem1.js
@@ -0,0 +1,7 @@
+// "typeof" bebohar kore program likhe dekh, kono variabler man ki dhoroner\ dhor, ekri variable "price" declare kor, jar maan "555",akhon console-a ai type check kor
+
+// ANS
+
+const price = 555;
+
+console.log(typeof price);
