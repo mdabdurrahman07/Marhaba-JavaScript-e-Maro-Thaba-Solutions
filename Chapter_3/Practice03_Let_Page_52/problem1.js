d1a9d4c15660831a8a0a2992107f70ea9f8817ce
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:53:47 +0600

    Chapter 3 Let Practice 1 solution

---
+++ Chapter_3/Practice03_Let_Page_52/problem1.js
@@ -0,0 +1,11 @@
+// tor bortoman boyosh age name ekti variable a rakh | console print kor | tarpor age variable notun maan set kor abong abar console print kor|
+
+// ANS
+
+let age = 23;
+
+console.log(age);
+
+age = 24;
+
+console.log(age);
