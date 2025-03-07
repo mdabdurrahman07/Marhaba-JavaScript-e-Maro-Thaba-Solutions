7695bd66d6166a01a207b9fc2f6146d51137ab88
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:34:37 +0600

    Chapter 3 Typeof Practice 5 solution

---
+++ Chapter_3/Practice03_Typeof_Page_50/problem5.js
@@ -0,0 +1,5 @@
+// khidar chote tui "isHungry" name ekti variable banai felli, ar shetar maan true| akhon console.log() ar "typeof" bebohar kore er type dekh|
+
+const isHungry = true;
+
+console.log(typeof isHungry);
