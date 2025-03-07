1587b944117e5c1f04c293f337de74c63f03b698
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-07 15:27:31 +0600

    Chapter Typeof Practice 3 solution

---
+++ Chapter_3/Practice03_Typeof_Page_50/problem3.js
@@ -0,0 +1,7 @@
+// megaCity naok ekti variabler modhe maan hisabe "Dhaka" aache \ aibar "typeof" bebohar kore dekh, ai variableta ki dhoroner | 
+
+// ANS
+
+const megaCity = "Dhaka";
+
+console.log(typeof megaCity);
