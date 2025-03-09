0fb9a3a1f37a892be9e8ec0455d2184640bddee1
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-09 23:59:34 +0600

    Chapter 3 Keyword Practice 5 Solution

---
+++ Chapter_3/Practice03_Keyword_Page_59/problem5.js
@@ -0,0 +1,15 @@
+// 'super' is not allowed as a variable declaration name
+
+// const super = "27";
+
+// console:-  SyntaxError: Unexpected token 'super'
+
+// Correct useCase
+
+const superOver = 27;
+
+
+
+
+
+
