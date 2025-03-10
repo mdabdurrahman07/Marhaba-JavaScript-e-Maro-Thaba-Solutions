6e2e34b44cfdf11804a58d789a1d9e4fb0eff92f
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-11 00:03:14 +0600

    Chapter 3 Parse & NaN Practice 6 Solution

---
+++ Chapter_3/Practice03_Parse_NaN_Page_64/problem6.js
@@ -0,0 +1,11 @@
+const valueOne = "10.5678";
+
+const valueTwo = "5.4321";
+
+const sum = parseFloat(valueOne) + parseFloat(valueTwo);
+
+console.log(sum);
+
+const result = parseFloat(sum.toFixed(1));
+
+console.log(result);
