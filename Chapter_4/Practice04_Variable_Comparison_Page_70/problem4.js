c9590da3017f70e25df9bbdf98d19967199e0242
mdabdurrahman07 <jamilrahman2001@gmail.com>
2025-03-17 23:57:53 +0600

    Chapter 4 Variable  Comparison  Practice 4,5  Solution

---
+++ Chapter_4/Practice04_Variable_Comparison_Page_70/problem4.js
@@ -0,0 +1,6 @@
+const myself = 10;
+
+const myFriend = 10;
+
+console.log(myself == myFriend);
+
---
+++ Chapter_4/Practice04_Variable_Comparison_Page_70/problem5.js
@@ -0,0 +1,5 @@
+const myBiscuit = 15;
+
+const myFriendBiscuit = 20;
+
+console.log(myFriendBiscuit > myBiscuit);
