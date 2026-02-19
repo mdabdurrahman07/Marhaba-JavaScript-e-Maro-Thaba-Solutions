e73162436cc110d0a663f14966a51c1b4bf3567f
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-02-19 14:20:32 +0600

    Chapter 8 objects page 126 problem 1 & 2 solved

---
+++ Chapter_8/Practice08_Object_Page_126_127/problem1.js
@@ -0,0 +1,8 @@
+const teacher = {
+    name: "Jamil",
+    subject: "ICT",
+    age: 24,
+    address: "Dhaka",
+    married: false
+}
+console.log(teacher)
---
+++ Chapter_8/Practice08_Object_Page_126_127/problem2.js
@@ -0,0 +1,7 @@
+const tree = {
+    name: "Mango Tree",
+    height: 15,
+    age: 20,
+    fruit: "mango"
+}
+console.log(tree)
