2cfe574b58c7d821e566ad22d92dfff43c70ba52
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-02-28 02:32:19 +0600

    Object dot notation problem8 solved

---
+++ Chapter_8/Practice08_Object_Dot_Bracket_Notation_Page_131_132/problem8.js
@@ -0,0 +1,14 @@
+const  family = {
+    father : {
+       name: "Habib",
+       age: 60,
+       profession: "job"
+    },
+    mother: {
+        name: "Bina",
+        age: 55,
+        profession: "houseWife"
+    }
+};
+
+console.log(family.father.age + family.mother.age);
